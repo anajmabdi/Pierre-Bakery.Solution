916be613beae3d6132c2f3d7c4fa3dd8c3756023
Najma Abdi <anajmabdi@gmail.com>
2022-12-09 15:35:33 -0800

    calculating cost of up to 2 loafs of bread

--- PierreBakery.Tests/Models.Test/BreadTests.cs
+++ PierreBakery.Tests/Models.Test/BreadTests.cs
@@ -7,10 +7,32 @@ namespace PierresBakery.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void BreadConstructor_CreatesInstanceOfItem_Item()
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-    Bread newBread = new Bread();
-    Assert.AreEqual(typeof(Bread), newBread.GetType());
+      Bread newBread = new Bread(1);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+    [TestMethod]
+    public void GetAmount_Return1_Int()
+    {
+      //Arrange
+      int bread = 1;
+      Bread newBread = new Bread(bread);
+      //Act
+      int result = newBread.BreadAmount; 
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+    [TestMethod]
+    public void SetCost_OneLoafCost_BreadTotal() {
+      //Arrange
+      Bread newBread = new Bread(1);
+
+      //Act
+      int result = newBread.BreadTotal();
+
+      //Assert
+      Assert.AreEqual(5, result);
     }
   }
 }
--- PierreBakery/Models/Bread.cs
+++ PierreBakery/Models/Bread.cs
@@ -2,6 +2,24 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    
+    public int BreadAmount { get; set; }
+
+
+    public Bread(int breadAmount)
+    {
+      BreadAmount = breadAmount;
+    }
+    public int BreadTotal()
+    {
+      if (BreadAmount <= 2)
+      {
+        int price = BreadAmount * 5;
+        return price;
+      }
+      else
+      {
+        return 3;
+      }
+    }
   }
 }
