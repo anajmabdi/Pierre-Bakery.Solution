b7a9392e5309d76b5fc6fe6b564d264a5836fded
Najma Abdi <anajmabdi@gmail.com>
2022-12-09 21:00:56 -0800

    create Pastry class and constructor

---
+++ PierreBakery.Tests/Models.Test/PastryTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
---
+++ PierreBakery/Models/Pastry.cs
@@ -0,0 +1,13 @@
+namespace PierresBakery.Models
+{
+  public class Pastry
+  {
+    public int PastryAmount { get; set; }
+
+
+    public Pastry(int pastryAmount)
+    {
+      PastryAmount = pastryAmount;
+    }
+  }
+}
